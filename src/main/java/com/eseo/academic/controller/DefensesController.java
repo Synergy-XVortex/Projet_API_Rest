c63791cd50a2f09b64a43337600350e040137d91
Clément Vongsanga <clement.vongsanga@gmail.com>
2026-05-25 11:44:16 +0200

    feat: add CrossOrigin support and scheduleDefense endpoint in DefensesController

--- src/main/java/com/eseo/academic/controller/DefensesController.java
+++ src/main/java/com/eseo/academic/controller/DefensesController.java
@@ -5,12 +5,17 @@ import java.util.List;
 import org.openapitools.api.DefensesApi;
 import org.openapitools.model.DefenseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.eseo.academic.service.DefenseService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173") 
 public class DefensesController implements DefensesApi {
 
     @Autowired
@@ -20,5 +25,11 @@ public class DefensesController implements DefensesApi {
     public ResponseEntity<List<DefenseDTO>> getDefenses() {
         List<DefenseDTO> defenses = defenseService.getAllDefenses();
         return ResponseEntity.ok(defenses);
+    }
+
+    @PostMapping("/defenses")
+    public ResponseEntity<Void> scheduleDefense(@RequestBody DefenseDTO defenseDTO) {
+        defenseService.scheduleDefense(defenseDTO);
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 }
